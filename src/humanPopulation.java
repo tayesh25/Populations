0799a400ddebe238cb88e5c31c77f1f0e1b658d5
tayesh25 <tayesh25@sciencleadership.org>
2023-10-13 10:24:36 -0400

    Initial commit

---
+++ src/humanPopulation.java
@@ -0,0 +1,18 @@
+public class humanPopulation extends Animal{
+    private String country;
+
+        private long populationSize;
+
+        private int percentChange;
+
+        public humanPopulation(String country, long populationSize, int percentChange) {
+            this.country = country;
+            this.populationSize = populationSize;
+            this.percentChange = percentChange;
+        }
+
+
+
+
+    }
+}
