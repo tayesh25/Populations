4a5358e5e3cf97e981ecc51b8dba47f76098d5cb
tayesh25 <tayesh25@sciencleadership.org>
2023-10-31 14:34:34 -0400

    fix

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws Exception {
         ArrayList<humanPopulation> humanPopulationArrayList = new ArrayList<>();
-        //humanPopulation.readData();
+        humanPopulation.readData();
         ArrayList<tortoisePopulation> tortoisePopulationArrayList = new ArrayList<>();
         tortoisePopulation.readData();
 
--- src/tortoisePopulation.java
+++ src/tortoisePopulation.java
@@ -1,27 +1,12 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class tortoisePopulation extends Animal {
-    private long populationSize1;
 
-    private long populationSize2;
-    private long populationSize3;
-    private long populationSize4;
-    private long populationSize5;
-    private long populationSize6;
-    private long populationSize7;
-    private long populationSize8;
-    private long populationSize9;
-
-
-
-    tortoisePopulation(long populationSize) {
+    public tortoisePopulation(String area) {
         this.area = area;
-        super.populationSize = populationSize;
-        this.percentChange = percentChange;
     }
-
-
 
     public String getArea() {
         return area;
@@ -31,41 +16,18 @@ public class tortoisePopulation extends Animal {
         this.area = area;
     }
 
-    public String toString() {
-        return
-                "Area:'" + area + '\'' +
+    private String area;
 
-                        ", PopulationSize: " + populationSize +
-                        ", PercentChange: " + percentChange +
-                        ' ';
-    }
+
     public static void readData() throws Exception {
         File dataFile = new File("src/tortoisePopulationDATA");
         Scanner dataScanner = new Scanner(dataFile);
-        dataScanner.useDelimiter(":");
-
-        String titleLine = dataScanner.nextLine();
-        Scanner titleScanner = new Scanner(titleLine);
-        titleScanner.useDelimiter(":");
-        //Tortoise Conservation Area Acr Area (km2):2004:2005:2007:2008:2009:2010:2011:2012:2013:2014
-        // read years
-        String titleData1 = titleScanner.next();
-        long year1 = titleScanner.nextLong();
-        long year2 = titleScanner.nextLong();
-        long year3 = titleScanner.nextLong();
-        long year4 = titleScanner.nextLong();
-        long year5 = titleScanner.nextLong();
-        long year6 = titleScanner.nextLong();
-        long year7 = titleScanner.nextLong();
-        long year8 = titleScanner.nextLong();
-        long year9 = titleScanner.nextLong();
-
+        dataScanner.useDelimiter("\t");
         while (dataScanner.hasNext()) {
-            //Colorado Desert:13,530:3,319:3,984:2,007:1,348:1,375:2,383:1,316:1,403
             String area = dataScanner.next();
-            long pop1 = dataScanner.nextLong();
-            tortoisePopulation tortoisePopulation1 = new tortoisePopulation();
-           // System.out.println(tortoisePopulation1);
+
+
+
         }
     }
 
